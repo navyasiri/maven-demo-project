f876535c257346eb2ee1ecd810caa7045a32f00f
navya <navya@example.com>
2021-07-11 11:09:22 +0000

    fixed the workspace issue

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,6 @@ echo "******************************"
 echo "****Building jar**************"
 echo "******************************"
 
-docker run --rm  -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:latest "$@"
+WORKSPACE=/home/ec2-user/jenkins_home/jenkins_home/workspace/pipeline-maven-project-demo
+
+docker run --rm  -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:latest "$@"
