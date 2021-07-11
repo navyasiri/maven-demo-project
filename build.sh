84a09766f410d3aeeff17d8d529eada0b1e782ee
navya <navya@example.com>
2021-07-11 13:08:04 +0000

    oppssss

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp -f java-app/target/*.jar jenkins/build/
-
-cd jenkins/build && docker-compose docker-compose.yml build --no-cache
--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -2,7 +2,6 @@
 
 cp -f java-app/target/*.jar jenkins/build/
 
-export BUILD_TAG=$1
 cd jenkins/build/ && docker-compose -f docker-compose.yml build --no-cache
 
 
